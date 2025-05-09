f8d06ce41482e87c4bad32780bcff7ed11f152f6
unknown <Cetecom@SJAPCA03LC1030.duoc.acad>
2025-05-09 15:49:17 -0400

    init

---
+++ src/main/java/com/example/bibliotecaduoc/services/LibroService.java
@@ -0,0 +1,12 @@
+package com.example.bibliotecaduoc.services;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class LibroService {
+
+}
